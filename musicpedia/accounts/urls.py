985583c9a2bbecd80c35a4a2a9a430c1a4cd09a9
Deyan Altanov <96491086+DeyanAltanov@users.noreply.github.com>
2023-08-20 05:58:05 +0300

    Delete musicpedia directory

--- musicpedia/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from musicpedia.accounts.views import login_user, logout_user, RegisterView
-
-urlpatterns = (
-    path('login/', login_user, name='log in user'),
-    path('logout/', logout_user, name='log out user'),
-    path('register/', RegisterView.as_view(), name='register user'),
-)
