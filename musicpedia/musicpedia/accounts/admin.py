a7881d967227544bbee9fe253412676ecdca88ce
Deyan Altanov <96491086+DeyanAltanov@users.noreply.github.com>
2023-08-20 08:33:31 +0300

    Add files via upload

---
+++ musicpedia/musicpedia/accounts/admin.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from musicpedia.accounts.models import Profile
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class MusicpediaUserAdmin(UserAdmin):
+    list_display = ('email', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Permissions"), {
+                "fields": (
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    readonly_fields = ('date_joined',)
